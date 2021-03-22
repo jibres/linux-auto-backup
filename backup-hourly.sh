5df5269982d189e4fce9635bb0ed045b940ba018
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-23 03:45:20 +0430

    some fix on hourly backup and save to s3

--- backup-hourly.sh
+++ backup-hourly.sh
@@ -1,28 +1,41 @@
 # Simple bash script to backup sql database into file with mysqldump and transfer it to another server
 
 #define server name
-ZONE=$HOSTNAME
+ZONE=`hostname`
 #ZONE=YOUR_SERVER_NAME
 
 #define file name
-FILENAME=backup-$ZONE-db-$(date +%H).sql.gz
+FILENAME=backup[$ZONE]alldb-$(date +%H).sql.gz
 
 #define folder name
-BACKUP_FOLDER=hourly
+BACKUP_FOLDER="hourly"
 
 #define file path
-FILEPATH=$BACKUP_FOLDER/$FILENAME
+FOLDER_PATH="$(pwd)/$BACKUP_FOLDER"
+# create folder if not exist
+mkdir -p $FOLDER_PATH
+# create file full path
+FILEPATH=$FOLDER_PATH/$FILENAME
 
 #define remote server addr
 TARGET=root@1.2.3.4
 
-TARGET_PATH=$ZONE/mysql-auto-backup/$BACKUP_FOLDER
+#define remote server path
+TARGET_FOLDER=/mysql-auto-backup/$ZONE/$BACKUP_FOLDER/
+TARGET_PATH=/home$TARGET_FOLDER
+
+
+
 
 # create a dump from all database
-mysqldump --quick --single-transaction --column-statistics=0 --all-databases | gzip > $FILEPATH
+echo "*** START DUMP DATABASE"
+#mysqldump --quick --single-transaction --column-statistics=0 --all-databases | gzip > $FILEPATH
 
 # sync with remote server
-rsync -avrt $FILEPATH $TARGET:/home/$TARGET_PATH/
+echo "*** SYNC WITH TARGET SERVER"
+rsync -avrt --rsync-path="mkdir -p $TARGET_PATH && rsync" $FILEPATH $TARGET:$TARGET_PATH
 
-#sync with s3 compatible storage
-s3cmd sync $BACKUP_FOLDER/ s3://YOUR_BUCKET/$TARGET_PATH/
+echo "*** SYNC WITH S3 STORAGE"
+s3cmd sync $FILEPATH s3://talambar$TARGET_FOLDER
+
+echo "*** Mission Complete"
