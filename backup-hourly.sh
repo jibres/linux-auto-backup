05d70c7f923f887bcdb38edb593906f9e3f7530d
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-23 06:31:51 +0430

    successfully get backup and transfer to server & s3

--- backup-hourly.sh
+++ backup-hourly.sh
@@ -1,13 +1,24 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+# shellcheck disable=SC1091
+#
+# Configure
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"
+# include yaml reader script
+source script/yaml.sh
+#
 # Simple bash script to backup sql database into file with mysqldump and transfer it to another server
+#
+#
+# Execute yaml reader
+create_variables conf/config[$(hostname)].me.yaml
+
 
 #define server name
-ZONE=`hostname`
-
-if [ -f conf/hostname.me.conf ]; then
-	ZONE=`cat conf/hostname.me.conf`
-	ZONE=$ZONE|xargs
+if [ ! $server_name ]; then
+	server_name=`hostname`
 fi
-#ZONE=YOUR_SERVER_CUSTOM_NAME
 
 
 # check if need to transfer to bucket 1
@@ -18,32 +29,9 @@ fi
 #BUCKET_1=YOUR_BUCKET_NAME
 
 
-# check if need to transfer to baclup server
-if [ -f conf/backup_server_1.me.conf ]; then
-	BACKUP_SERVER_1=`cat conf/backup_server_1.me.conf`
-	BACKUP_SERVER_1=$BACKUP_SERVER_1|xargs
-fi
-#BACKUP_SERVER_1=root@1.2.3.4
-
-
-# check if need to transfer to baclup server
-if [ -f conf/backup_server_2.me.conf ]; then
-	BACKUP_SERVER_2=`cat conf/backup_server_2.me.conf`
-	BACKUP_SERVER_2=$BACKUP_SERVER_2|xargs
-fi
-#BACKUP_SERVER_2=root@1.2.3.4
-
-
-# check if need to transfer to baclup server
-if [ -f conf/backup_server_3.me.conf ]; then
-	BACKUP_SERVER_3=`cat conf/backup_server_3.me.conf`
-	BACKUP_SERVER_3=$BACKUP_SERVER_3|xargs
-fi
-#BACKUP_SERVER_3=root@1.2.3.4
-
 
 #define file name
-FILENAME=backup[$ZONE]-alldb-h$(date +%H).sql.gz
+FILENAME=backup[$server_name]-alldb-h$(date +%H).sql.gz
 
 #define folder name
 BACKUP_FOLDER="hourly"
@@ -56,7 +44,7 @@ mkdir -p $FOLDER_PATH
 FILEPATH=$FOLDER_PATH/$FILENAME
 
 #define remote server path
-TARGET_FOLDER=/mysql-auto-backup/$ZONE/$BACKUP_FOLDER/
+TARGET_FOLDER=/mysql-auto-backup/$server_name/$BACKUP_FOLDER/
 TARGET_PATH=/home$TARGET_FOLDER
 
 
@@ -66,35 +54,45 @@ echo "*** START DUMP DATABASE"
 mysqldump --quick --single-transaction --column-statistics=0 --all-databases | gzip > $FILEPATH
 
 
-if [ $BACKUP_SERVER_1 ]; then
+# transfer backup to server if set
+if [ $backup_server1 ]; then
 	# sync with remote server 1
 	echo "*** SYNC WITH TARGET SERVER 1"
-	echo $BACKUP_SERVER_1:$TARGET_PATH
-	rsync -avrt --rsync-path="mkdir -p $TARGET_PATH && rsync -avrt" $FILEPATH $BACKUP_SERVER_1:$TARGET_PATH
+	echo "--> PATH "$backup_server1:$TARGET_PATH
+	rsync -avrt --rsync-path="mkdir -p $TARGET_PATH && rsync -avrt" $FILEPATH $backup_server1:$TARGET_PATH
 fi
 
 
-if [ $BACKUP_SERVER_2 ]; then
+if [ $backup_server2 ]; then
 	# sync with remote server 2
 	echo "*** SYNC WITH TARGET SERVER 2"
-	echo $BACKUP_SERVER_2:$TARGET_PATH
-	rsync -avrt --rsync-path="mkdir -p $TARGET_PATH && rsync -avrt" $FILEPATH $BACKUP_SERVER_2:$TARGET_PATH
+	echo "--> PATH "$backup_server2:$TARGET_PATH
+	rsync -avrt --rsync-path="mkdir -p $TARGET_PATH && rsync -avrt" $FILEPATH $backup_server2:$TARGET_PATH
 fi
 
 
-if [ $BACKUP_SERVER_3 ]; then
+if [ $backup_server3 ]; then
 	# sync with remote server 3
 	echo "*** SYNC WITH TARGET SERVER 3"
-	echo $BACKUP_SERVER_3:$TARGET_PATH
-	rsync -avrt --rsync-path="mkdir -p $TARGET_PATH && rsync -avrt" $FILEPATH $BACKUP_SERVER_3:$TARGET_PATH
+	echo "--> PATH "$backup_server3:$TARGET_PATH
+	rsync -avrt --rsync-path="mkdir -p $TARGET_PATH && rsync -avrt" $FILEPATH $backup_server3:$TARGET_PATH
 fi
 
-
-if [ $BUCKET_1 ]; then
+# transfer backup to s3
+# first use system s3 with saved detail
+if [ $s3_bucketSaved_name ]; then
 	# sync with s3 storage server 1
-	echo "*** SYNC WITH S3 STORAGE"
-	echo s3://$BUCKET_1$TARGET_FOLDER
-	s3cmd sync $FILEPATH s3://$BUCKET_1$TARGET_FOLDER
+	echo "*** SYNC WITH S3 STORAGE 0 - credential from config"
+	echo "--> PATH "s3://$s3_bucketSaved_name$TARGET_FOLDER
+	s3cmd sync $FILEPATH s3://$s3_bucketSaved_name$TARGET_FOLDER
+fi
+
+# save into 3 different s3 storage with pass credential detail
+if [ $s3_bucket1_name ] && [ $s3_bucket1_access ] && [ $s3_bucket1_secret ] && [ $s3_bucket1_endpoint ]; then
+	# sync with s3 storage server 1
+	echo "*** SYNC WITH S3 STORAGE 1"
+	echo "--> PATH "s3://$s3_bucket1_name$TARGET_FOLDER
+	#s3cmd sync $FILEPATH s3://$s3_bucket1_name$TARGET_FOLDER
 fi
 
 echo "*** Mission Complete"
