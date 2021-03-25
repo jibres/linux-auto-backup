c3c522d33c86d563cb821f5137a4bf7d79ebb7f8
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-25 23:28:21 +0430

    add backup + sync cmd

---
+++ backup-monthly+sync.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+# shellcheck disable=SC1091
+
+#define backup type
+BACKUP_FOLDER="monthly"
+
+# call backup script
+source start-backup-from-all-databases.sh
+# sync backup
+source sync-changes.sh
+# done.
