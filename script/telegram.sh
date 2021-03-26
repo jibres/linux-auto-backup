bb96d3a8cc83e665261fda1e2edddcb5099c38c4
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-26 06:53:39 +0430

    improve telegram script

--- script/telegram.sh
+++ script/telegram.sh
@@ -4,37 +4,30 @@
 
 telegram_send() {
 
-telegram_bot_token="$telegram_token"
-telegram_chat_id="$telegram_chatid"
-telegram_broker_url="$telegram_broker"
-telegram_broker_token="$telegram_brokertoken"
+if [ $telegram_token ]; then
 
-echo $telegram_bot_token
-echo $telegram_chat_id
-echo $telegram_broker_url
-echo $telegram_broker_token
-
-if [ $telegram_broker_url ]; then
+if [ $telegram_broker ]; then
 echo "send to telegram broker"
 
 curl \
--H X-TG-TOKEN:"$telegram_bot_token" \
--H BROKER-TOKEN:"$telegram_broker_token" \
+-H X-TG-TOKEN:"$telegram_token" \
+-H BROKER-TOKEN:"$telegram_brokertoken" \
 -d parse_mode="HTML" \
--d chat_id="$telegram_chat_id" \
+-d chat_id="$telegram_chatid" \
 -d text="$1" \
 -d method="sendMessage" \
-$telegram_broker_url
+$telegram_broker
 
 else
 echo "send to telegram api"
 
 curl \
 -d parse_mode="HTML" \
--d chat_id="$telegram_chat_id" \
+-d chat_id="$telegram_chatid" \
 -d text="$1" \
-https://api.telegram.org/bot$telegram_bot_token/sendMessage
+https://api.telegram.org/bot$telegram_token/sendMessage
 
 fi
 
+fi
 }
