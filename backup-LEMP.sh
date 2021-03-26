5b54b67cb2985bbcc1f0c328e2ace9d2d0862a21
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-27 03:00:03 +0430

    create backup from LEMP folders if exist

--- backup-LEMP.sh
+++ backup-LEMP.sh
@@ -2,13 +2,33 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 # create backup folder
 mkdir -p LEMP
+
 # copy nginx
-rsync -at --no-links /etc/nginx LEMP/ | grep -v "skipping non-regular file"
+if [ -d "/etc/nginx/" ] 
+then
+	rsync -at --no-links /etc/nginx LEMP/ | grep -v "skipping non-regular file"
+fi
+
 # copy php
-rsync -at --no-links /etc/php LEMP/ | grep -v "skipping non-regular file"
+if [ -d "/etc/php/" ] 
+then
+	rsync -at --no-links /etc/php LEMP/ | grep -v "skipping non-regular file"
+fi
+
 # copy MySQL
-rsync -at --no-links /etc/mysql LEMP/ | grep -v "skipping non-regular file"
+if [ -d "/etc/mysql/" ] 
+then
+	rsync -at --no-links /etc/mysql LEMP/ | grep -v "skipping non-regular file"
+fi
+
 # copy ssl
-rsync -at --no-links /etc/ssl LEMP/ | grep -v "skipping non-regular file"
+if [ -d "/etc/ssl/" ] 
+then
+	rsync -at --no-links /etc/ssl LEMP/ | grep -v "skipping non-regular file"
+fi
+
 # copy www folder
-rsync -at --no-links /var/www LEMP/ | grep -v "skipping non-regular file"
+if [ -d "/etc/www/" ] 
+then
+	rsync -at --no-links /var/www LEMP/ | grep -v "skipping non-regular file"
+fi
